7a5ae6803626dad0a3ca5b961eb2f2c3157dd24e
ponraj mani <ponrajmani1997@gmail.com>
2021-08-16 12:46:40 +0530

    first commit

---
+++ CARTOONS/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def TOM(request):
+    return HttpResponse('BEST SHOW IN 90S')
+
+def SHINCHAN(request):
+    return HttpResponse('BEST SHOW NOW')    
